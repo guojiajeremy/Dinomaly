75f107b830ee26528aea5861b763eb99c63dd913
guojiajeremy <guojia.jeremy@gmail.com>
2024-05-28 15:31:51 +0800

    init version

--- dinomaly_mpdd_sep.py
+++ dinomaly_mpdd_sep.py
@@ -15,7 +15,8 @@ from torch.utils.data import DataLoader, ConcatDataset
 from models.uad import ViTill, ViTillv2
 from models import vit_encoder
 from dinov1.utils import trunc_normal_
-from models.vision_transformer import Block as VitBlock, Mlp, bMlp, EfficientBlock
+from models.vision_transformer import Block as VitBlock, bMlp, Attention, LinearAttention, \
+    LinearAttention2
 from dataset import MVTecDataset
 import torch.backends.cudnn as cudnn
 import argparse
@@ -23,7 +24,7 @@ from utils import evaluation_batch, global_cosine, replace_layers, global_cosine
 from torch.nn import functional as F
 from functools import partial
 from ptflops import get_model_complexity_info
-
+from optimizers import StableAdamW
 import warnings
 import copy
 import logging
@@ -82,8 +83,8 @@ def train(item):
 
     data_transform, gt_transform = get_data_transforms(image_size, crop_size)
 
-    train_path = '../MPDD/' + item + '/train'
-    test_path = '../MPDD/' + item
+    train_path = os.path.join(args.data_path, item, 'train')
+    test_path = os.path.join(args.data_path, item)
 
     train_data = ImageFolder(root=train_path, transform=data_transform)
     test_data = MVTecDataset(root=test_path, transform=data_transform, gt_transform=gt_transform, phase="test")
@@ -91,8 +92,14 @@ def train(item):
                                                    drop_last=True)
     test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=4)
 
+    # encoder_name = 'dinov2reg_vit_small_14'
     encoder_name = 'dinov2reg_vit_base_14'
+    # encoder_name = 'dinov2reg_vit_large_14'
+
     target_layers = [2, 3, 4, 5, 6, 7, 8, 9]
+    fuse_layer_encoder = [[0, 1, 2, 3], [4, 5, 6, 7]]
+    fuse_layer_decoder = [[0, 1, 2, 3], [4, 5, 6, 7]]
+    # target_layers = list(range(4, 19))
 
     encoder = vit_encoder.load(encoder_name)
 
@@ -102,8 +109,10 @@ def train(item):
         embed_dim, num_heads = 768, 12
     elif 'large' in encoder_name:
         embed_dim, num_heads = 1024, 16
+        target_layers = [4, 6, 8, 10, 12, 14, 16, 18]
     else:
         raise "Architecture not in small, base, large."
+
     bottleneck = []
     decoder = []
 
@@ -111,37 +120,34 @@ def train(item):
     bottleneck = nn.ModuleList(bottleneck)
 
     for i in range(8):
-        # blk = VitBlock(dim=embed_dim, num_heads=num_heads, mlp_ratio=4.,
-        #                qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-8), attn_drop=0.3)
-        blk = EfficientBlock(dim=embed_dim, num_heads=num_heads, mlp_ratio=4.,
-                             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-8))
+        blk = VitBlock(dim=embed_dim, num_heads=num_heads, mlp_ratio=4.,
+                       qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-8), attn_drop=0.,
+                       attn=LinearAttention2)
         decoder.append(blk)
     decoder = nn.ModuleList(decoder)
 
     model = ViTill(encoder=encoder, bottleneck=bottleneck, decoder=decoder, target_layers=target_layers,
-                   fuse_dropout=0.)
+                   mask_neighbor_size=0, fuse_layer_encoder=fuse_layer_encoder, fuse_layer_decoder=fuse_layer_decoder)
     model = model.to(device)
     trainable = nn.ModuleList([bottleneck, decoder])
 
     for m in trainable.modules():
         if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=0.02, a=-0.08, b=0.08)
+            trunc_normal_(m.weight, std=0.01, a=-0.03, b=0.03)
             if isinstance(m, nn.Linear) and m.bias is not None:
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, nn.LayerNorm):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
-    optimizer = torch.optim.AdamW([{'params': decoder.parameters()}, {'params': bottleneck.parameters()}],
-                                  lr=3e-3, betas=(0.9, 0.999), weight_decay=1e-5, amsgrad=True, eps=1e-10)
-    lr_scheduler = WarmCosineScheduler(optimizer, base_value=3e-3, final_value=3e-4, total_iters=total_iters,
+    optimizer = StableAdamW([{'params': trainable.parameters()}],
+                            lr=2e-3, betas=(0.9, 0.999), weight_decay=1e-4, amsgrad=True, eps=1e-8)
+    lr_scheduler = WarmCosineScheduler(optimizer, base_value=2e-3, final_value=2e-4, total_iters=total_iters,
                                        warmup_iters=100)
 
     print_fn('train image number:{}'.format(len(train_data)))
 
     it = 0
-    auroc_px_best, auroc_sp_best, ap_px_best, ap_sp_best = 0, 0, 0, 0
-
     for epoch in range(int(np.ceil(total_iters / len(train_dataloader)))):
         model.train()
 
@@ -151,10 +157,9 @@ def train(item):
             label = label.to(device)
 
             en, de = model(img)
-            # loss = global_cosine(en, de)
 
             p_final = 0.9
-            p = min(p_final * it / (total_iters * 0.1), p_final)
+            p = min(p_final * it / 1000, p_final)
             loss = global_cosine_hm_percent(en, de, p=p, factor=0.1)
 
             optimizer.zero_grad()
@@ -165,26 +170,25 @@ def train(item):
             loss_list.append(loss.item())
             lr_scheduler.step()
 
-            if (it + 1) % 1000 == 0:
-                auroc_px, auroc_sp, ap_px, ap_sp, gt_pr = evaluation_batch(model, test_dataloader, device,
-                                                                           max_ratio=0.01)
-                if auroc_sp >= auroc_sp_best:
-                    auroc_px_best, auroc_sp_best, ap_px_best, ap_sp_best = auroc_px, auroc_sp, ap_px, ap_sp
-                print_fn('{}: P-Auroc:{:.4f}, I-Auroc:{:.4f},P-AP:{:.4f},I-AP:{:.4f}'.format(item,
-                                                                                             auroc_px,
-                                                                                             auroc_sp,
-                                                                                             ap_px, ap_sp))
+            if (it + 1) % 5000 == 0:
+                results = evaluation_batch(model, test_dataloader, device, max_ratio=0.01, resize_mask=256)
+                auroc_sp, ap_sp, f1_sp, auroc_px, ap_px, f1_px, aupro_px = results
 
+                print_fn(
+                    '{}: I-Auroc:{:.4f}, I-AP:{:.4f}, I-F1:{:.4f}, P-AUROC:{:.4f}, P-AP:{:.4f}, P-F1:{:.4f}, P-AUPRO:{:.4f}'.format(
+                        item, auroc_sp, ap_sp, f1_sp, auroc_px, ap_px, f1_px, aupro_px))
                 model.train()
 
             it += 1
             if it == total_iters:
                 break
-        print_fn('iter [{}/{}], loss:{:.4f}'.format(it, total_iters, np.mean(loss_list)))
+            if (it + 1) % 100 == 0:
+                print_fn('iter [{}/{}], loss:{:.4f}'.format(it, total_iters, np.mean(loss_list)))
+                loss_list = []
 
     # torch.save(model.state_dict(), os.path.join(args.save_dir, args.save_name, 'model.pth'))
 
-    return auroc_px, auroc_sp, ap_px, ap_sp, auroc_px_best, auroc_sp_best, ap_px_best, ap_sp_best
+    return auroc_sp, ap_sp, f1_sp, auroc_px, ap_px, f1_px, aupro_px
 
 
 if __name__ == '__main__':
@@ -192,13 +196,15 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--data_path', type=str, default='../mvtec_anomaly_detection')
     parser.add_argument('--save_dir', type=str, default='./saved_results')
     parser.add_argument('--save_name', type=str,
-                        default='vitill_mpdd_sep_dinov2br_c392_en29_v2bn4dp2_de8_eattn_md_it10k_lr3e3_wd1e5_w1hcosa3e4_hmp09f01w01_b16_s1')
+                        default='vitill_mpdd_sep_dinov2br_c392_en29_bn4dp2_de8_elaelu_md2_i1_it10k_sadm2e3_wd1e4_w1hcosa_ghmp09f01w1k_b16_ev_s1')
     args = parser.parse_args()
 
     item_list = ['bracket_black', 'bracket_brown', 'bracket_white', 'connector', 'metal_plate', 'tubes']
 
+    # item_list = ['leather']
     logger = get_logger(args.save_name, os.path.join(args.save_dir, args.save_name))
     print_fn = logger.info
 
@@ -206,25 +212,21 @@ if __name__ == '__main__':
     print_fn(device)
 
     result_list = []
-    result_list_best = []
     for i, item in enumerate(item_list):
-        auroc_px, auroc_sp, ap_px, ap_sp, auroc_px_best, auroc_sp_best, ap_px_best, ap_sp_best = train(item)
-        result_list.append([item, auroc_px, auroc_sp, ap_px, ap_sp])
-        result_list_best.append([item, auroc_px_best, auroc_sp_best, ap_px_best, ap_sp_best])
+        auroc_sp, ap_sp, f1_sp, auroc_px, ap_px, f1_px, aupro_px = train(item)
+        result_list.append([item, auroc_sp, ap_sp, f1_sp, auroc_px, ap_px, f1_px, aupro_px])
 
-    mean_auroc_px = np.mean([result[1] for result in result_list])
-    mean_auroc_sp = np.mean([result[2] for result in result_list])
-    mean_ap_px = np.mean([result[3] for result in result_list])
-    mean_ap_sp = np.mean([result[4] for result in result_list])
+    mean_auroc_sp = np.mean([result[1] for result in result_list])
+    mean_ap_sp = np.mean([result[2] for result in result_list])
+    mean_f1_sp = np.mean([result[3] for result in result_list])
+
+    mean_auroc_px = np.mean([result[4] for result in result_list])
+    mean_ap_px = np.mean([result[5] for result in result_list])
+    mean_f1_px = np.mean([result[6] for result in result_list])
+    mean_aupro_px = np.mean([result[7] for result in result_list])
 
     print_fn(result_list)
-    print_fn('mean P-Auroc:{:.4f}, I-AUROC:{:.4f}, P-AP:{:.4}, I-AP:{:.4}'.format(mean_auroc_px, mean_auroc_sp,
-                                                                                  mean_ap_px, mean_ap_sp))
-
-    best_auroc_px = np.mean([result[1] for result in result_list_best])
-    best_auroc_sp = np.mean([result[2] for result in result_list_best])
-    best_ap_px = np.mean([result[3] for result in result_list_best])
-    best_ap_sp = np.mean([result[4] for result in result_list_best])
-    print_fn(result_list_best)
-    print_fn('best P-Auroc:{:.4f}, I-AUROC:{:.4f}, P-AP:{:.4}, I-AP:{:.4}'.format(best_auroc_px, best_auroc_sp,
-                                                                                  best_ap_px, best_ap_sp))
+    print_fn(
+        'Mean: I-Auroc:{:.4f}, I-AP:{:.4f}, I-F1:{:.4f}, P-AUROC:{:.4f}, P-AP:{:.4f}, P-F1:{:.4f}, P-AUPRO:{:.4f}'.format(
+            mean_auroc_sp, mean_ap_sp, mean_f1_sp,
+            mean_auroc_px, mean_ap_px, mean_f1_px, mean_aupro_px))
