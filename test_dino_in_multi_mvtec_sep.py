6a9efb426545813ee7433055da1ab2265f9f1186
Niyuta <cheparity@gmail.com>
2026-03-12 12:59:46 +0800

    fix: no clip config and multiple Vitill_test class

--- test_dino_in_multi_mvtec_sep.py
+++ test_dino_in_multi_mvtec_sep.py
@@ -130,6 +130,15 @@ def train(item):
             "norm": True,
             "trainable": False,
         },
+        "clip": {
+            "arch": "ViT-B-16",
+            "pretrained": "openai",
+            "fuse_layer_encoder": [[0, 1, 2, 3, 4, 5, 6, 7]],
+            "target_layers": [2, 3, 4, 5, 6, 7, 8, 9],
+            "n": 10,
+            "norm": True,
+            "trainable": False,
+        },
     }
     multi_encoder = MultiEncoder(encoder_configs=encoder_configs)
     # target_layers = list(range(4, 19))
